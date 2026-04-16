2309eada65443a51e0ca59fe6e911649444af5e2
Morshed <golammorshed004@gmail.com>
2026-04-16 21:40:45 +0600

    feat: implement Elysia API routes for room creation and messaging with Zod validation

--- app/api/[[...slugs]]/route.ts
+++ app/api/[[...slugs]]/route.ts
@@ -1,6 +1,8 @@
 import { redis } from "@/lib/redis";
 import { Elysia, t } from "elysia";
 import { nanoid } from "nanoid";
+import { authMiddleware } from "./auth";
+import z from "zod";
 
 // Room Route
 
@@ -18,8 +20,30 @@ const room = new Elysia({ prefix: "/room" }).post("/create", async () => {
   return { roomId };
 });
 
+// Message Route
+const message = new Elysia({ prefix: "/message" }).use(authMiddleware).post(
+  "/",
+  async ({ body, auth }) => {
+    const { roomId, isConnected, token } = auth;
+    const { sender, msg } = body;
+
+    const isRoomExist = await redis.exists(`room:${roomId}`);
+
+    if(!isRoomExist){
+      throw new Error("Room does not exist")
+    }
+  },
+  {
+    query: z.object({ roomId: z.string() }),
+    body: z.object({
+      sender: z.string().max(100),
+      msg: z.string().max(1000),
+    }),
+  },
+);
+
 // Main app route
-const app = new Elysia({ prefix: "/api" }).use(room);
+const app = new Elysia({ prefix: "/api" }).use(room).use(message);
 
 export type App = typeof app;
 export const GET = app.fetch;
